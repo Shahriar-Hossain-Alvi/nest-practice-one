33aa5353d71d30f86761b92566791d5fde3c3b8c
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-05-26 00:28:35 +0600

    created and new user to db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto/auth.dto';
 
 // 'auth' inside the @Controller is global prefix route like /auth/signup or /auth/signin
 @Controller('auth')
-
 export class AuthController {
   // pass the authService in the param to use the functions in controller
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   // make sign up route
   @Post('signup')
   //@Body grabs the json object from request body and passes it to controller after that dto holds the body and validate with the AuthDto type
-  signup(@Body() dto: { email: string, password: string }) {
+  signup(@Body() dto: { email: string; password: string }) {
     return this.authService.signup(dto.email, dto.password);
   }
 
